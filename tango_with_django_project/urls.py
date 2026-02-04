7ebb718c35da730ec0e3b04acf3c1a6fae7ea053
“Tejas <3049840k@student.gla.ac.uk>
2026-02-04 00:04:43 +0000

    ch03: add about view + url + navigation links

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rango import views
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-# The above maps any URLs starting with rango/ to be handled by rango.
+    path('topg/', include('topg.urls')),  
     path('admin/', admin.site.urls),
 ]
