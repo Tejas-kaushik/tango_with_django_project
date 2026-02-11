71235f41e97198185d1567f215422a808985cb8c
“Tejas <3049840k@student.gla.ac.uk>
2026-02-11 17:53:56 +0000

    ch04: templates + basic tests for rango

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RangoViewsTests(TestCase):
+    def test_index_page_loads(self):
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Rango says")
+
+    def test_about_page_loads(self):
+        response = self.client.get(reverse('rango:about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "about page.")
