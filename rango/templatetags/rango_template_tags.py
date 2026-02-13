5a9a4402e67e21fecb5ab92e017d40e4dba6b25b
“Tejas <3049840k@student.gla.ac.uk>
2026-02-13 16:11:03 +0000

    Use base template for add_category and add_page

---
+++ rango/templatetags/rango_template_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from rango.models import Category
+
+register = template.Library()
+
+@register.inclusion_tag('rango/categories.html')
+def get_category_list(current_category=None):
+    return {
+        'categories': Category.objects.order_by('-likes')[:5],
+        'current_category': current_category,
+    }
